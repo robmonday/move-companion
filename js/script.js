fe876a2d4cb7f90421707f6a283daabbe52cb573
Rob Monday <rob.monday@gmail.com>
2016-05-06 11:22:46 -0500

    Update NYT heading to change dynamically, clean up code

--- js/script.js
+++ js/script.js
@@ -25,8 +25,10 @@ function loadData() {
 
     //NYT API
 
+	$('#nytimes-header').text( "New York Times Articles for "+(cityStr) );
+
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-	url += '?' + $.param({  //TODO eventually need to make this dynamic
+	url += '?' + $.param({  
 	  'api-key': "e99c2223728e47de96cf38e642e3c7c7",
 	  'q': cityStr,
 	  'fq': "type_of_material:News",
@@ -36,10 +38,10 @@ function loadData() {
 	});
 	//console.log(url);
     $.getJSON(url, function(data){
-    	console.log(data.response.docs[0].headline.main);
-    	console.log(data.response.docs[0].snippet);
-    	console.log(data.response.docs[0].lead_paragraph);
-    	console.log(data.response.docs[0].web_url);
+    	// console.log(data.response.docs[0].headline.main);
+    	// console.log(data.response.docs[0].snippet);
+    	// console.log(data.response.docs[0].lead_paragraph);
+    	// console.log(data.response.docs[0].web_url);
 
 		var items = [];
 		$.each( data.response.docs, function( key, val ) {
