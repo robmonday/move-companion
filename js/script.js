274494a093e5e9b9dc931f194672d62c05e5b2a6
Rob Monday <rob.monday@gmail.com>
2016-05-06 11:14:24 -0500

    Add NYT API

--- js/script.js
+++ js/script.js
@@ -19,8 +19,42 @@ function loadData() {
     var fullAddress = streetStr + ' in ' + cityStr;
 
     $greeting.text('So you want to live at '+fullAddress+'?');
+
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+streetStr.split(" ").join("+")+','+cityStr;
     $body.append('<img class="bgimg" src='+streetviewUrl+'>');
+
+    //NYT API
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?' + $.param({  //TODO eventually need to make this dynamic
+	  'api-key': "e99c2223728e47de96cf38e642e3c7c7",
+	  'q': cityStr,
+	  'fq': "type_of_material:News",
+	  'sort': "newest",
+	  'page': 0,
+	  'callback': "svc_search_v2_articlesearch"
+	});
+	//console.log(url);
+    $.getJSON(url, function(data){
+    	console.log(data.response.docs[0].headline.main);
+    	console.log(data.response.docs[0].snippet);
+    	console.log(data.response.docs[0].lead_paragraph);
+    	console.log(data.response.docs[0].web_url);
+
+		var items = [];
+		$.each( data.response.docs, function( key, val ) {
+			items.push( "<li id='" 
+				+ key + "' class='article'><a href="
+				+ val.web_url +" target='_blank'>" 
+				+ val.headline.main + "</a><p>"
+				+ val.snippet +"</p></li>" 
+				);
+		});
+		 
+		$( "<ul/>", {"class": "my-new-list", html: items.join( "" )}
+			).appendTo( "#nytimes-articles" );
+
+    });
 
     return false;
 };
